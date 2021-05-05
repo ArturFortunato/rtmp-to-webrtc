069a73261772b47671eb257467a4ca092176adf7
Ubuntu <ubuntu@ip-172-31-36-112.eu-west-2.compute.internal>
2021-05-05 16:15:15 +0000

    trying to prevent stream from stopping

--- main.go
+++ main.go
@@ -1,77 +1,95 @@
-	package main
+package main
 
-	import (
-		"encoding/json"
-		"math/rand"
-		"net/http"
-		"time"
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
 
-		"github.com/pion/webrtc/v2"
-	)
+	"github.com/pion/webrtc/v2"
+)
 
-	func main() {
-		rand.Seed(time.Now().UTC().UnixNano())
-		
-		go startRTMPServer()
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
 
-		http.Handle("/", http.FileServer(http.Dir(".")))
-		http.HandleFunc("/createPeerConnection", createPeerConnection)
-		panic(http.ListenAndServe(":8080", nil))
+	go startRTMPServer()
+
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/createPeerConnection", createPeerConnection)
+	http.HandleFunc("/test", test)
+	panic(http.ListenAndServe(":8080", nil))
+}
+
+func test(w http.ResponseWriter, r *http.Request) {
+	log.Println("Testing")
+	w.Write([]byte("ABCD"))
+}
+
+func createPeerConnection(w http.ResponseWriter, r *http.Request) {
+	//TESTING=========================
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("panic occurred:", err)
+		}
+	}()
+	//TESTING=========================
+
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	peerConnection, err := webrtc.NewPeerConnection(webrtc.Configuration{})
+	if err != nil {
+		panic(err)
 	}
 
-	func createPeerConnection(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	videoTrack, err := peerConnection.NewTrack(webrtc.DefaultPayloadTypeH264, rand.Uint32(), "video", "pion")
+	if err != nil {
+		panic(err)
+	}
+	_, err = peerConnection.AddTrack(videoTrack)
+	if err != nil {
+		panic(err)
+	}
 
-		peerConnection, err := webrtc.NewPeerConnection(webrtc.Configuration{})
-		if err != nil {
-			panic(err)
-		}
+	audioTrack, err := peerConnection.NewTrack(webrtc.DefaultPayloadTypePCMA, rand.Uint32(), "audio", "pion")
+	if err != nil {
+		panic(err)
+	}
+	if _, err = peerConnection.AddTrack(audioTrack); err != nil {
+		panic(err)
+	}
 
-		videoTrack, err := peerConnection.NewTrack(webrtc.DefaultPayloadTypeH264, rand.Uint32(), "video", "pion")
-		if err != nil {
-			panic(err)
-		}
-		_, err = peerConnection.AddTrack(videoTrack)
-		if ; err != nil {
-			panic(err)
-		}
+	var offer webrtc.SessionDescription
 
-		audioTrack, err := peerConnection.NewTrack(webrtc.DefaultPayloadTypePCMA, rand.Uint32(), "audio", "pion")
-		if err != nil {
-			panic(err)
-		}
-		if _, err = peerConnection.AddTrack(audioTrack); err != nil {
-			panic(err)
-		}
+	if err := json.NewDecoder(r.Body).Decode(&offer); err != nil {
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
 
-		var offer webrtc.SessionDescription
-	
-		if err := json.NewDecoder(r.Body).Decode(&offer); err != nil {
-			return
-		}
+	if err := peerConnection.SetRemoteDescription(offer); err != nil {
+		panic(err)
+	}
 
-		if err := peerConnection.SetRemoteDescription(offer); err != nil {
-			panic(err)
-		}
+	answer, err := peerConnection.CreateAnswer(nil)
+	if err != nil {
+		panic(err)
+	} else if err = peerConnection.SetLocalDescription(answer); err != nil {
+		panic(err)
+	}
 
-		answer, err := peerConnection.CreateAnswer(nil)
-		if err != nil {
-			panic(err)
-		} else if err = peerConnection.SetLocalDescription(answer); err != nil {
-			panic(err)
-		}
+	response, err := json.Marshal(answer)
+	if err != nil {
+		panic(err)
+	}
 
-		response, err := json.Marshal(answer)
-		if err != nil {
-			panic(err)
-		}
+	if _, err := w.Write(response); err != nil {
+		panic(err)
+	}
 
-		w.Header().Set("Content-Type", "application/json")
-		if _, err := w.Write(response); err != nil {
-			panic(err)
-		}
-
+	if len(r.URL.Query()["eventID"]) == 0 {
+		print(r.URL.Query())
+		panic("erro aqui")
+	} else {
 		eventID := r.URL.Query()["eventID"][0]
 		addNewClient(eventID, audioTrack, videoTrack)
 	}
+}
